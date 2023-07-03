f329f375ea8d8c87156601e3badd649436ea1fd3
palhiman <palhimanshu786@gmail.com>
2023-07-03 19:11:16 +0530

    Variable & Names

---
+++ Python3_Hardway/ex5.py
@@ -0,0 +1,14 @@
+my_name = "Shekhar"
+my_age = 27 # I think so
+my_height = 64.96 # inches
+my_weight = 62 # Kgs
+my_hair = 'black'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"He's {my_weight} kgs heavy.")
+print(f"He's got {my_hair} hair.")
+
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
+
