6c1a1a757f0d801f8ef2fe49038131befdd4a538
palhiman <palhimanshu786@gmail.com>
2023-07-06 00:32:38 +0530

    Asking questions

---
+++ Python3_Hardway/ex11.py
@@ -0,0 +1,11 @@
+# Exercise 11: Asking Questions
+
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end=' ')
+height = input()
+print("How much do you weigh?", end=' ')
+weight = input()
+
+print(f"So, you're {age} years old, {height} cms tall and {weight} kgs heavey...")
+print("You have maintained your body very well.")
