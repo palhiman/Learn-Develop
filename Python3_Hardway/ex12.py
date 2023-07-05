8cbe82eecc558e2dddf458143f6da60f92548a51
palhiman <palhimanshu786@gmail.com>
2023-07-06 00:32:58 +0530

    prompting people

---
+++ Python3_Hardway/ex12.py
@@ -0,0 +1,7 @@
+# Exercise 12: Prompting People
+
+age = input("How old are you? ")
+height = input("HOw tall are you? ")
+weight = input("How much do you weigh? ")
+
+print(f"So, you're {age} years old, {height} cms tall and {weight} kgs heavey...")
